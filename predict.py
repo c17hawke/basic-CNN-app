3005dd59dd8e8710c5d0bd28f58f9050e25df40e
c17hawke <sunny.c17hawke@gmail.com>
2020-12-05 12:34:03 +0530

    prediction file is update

--- predict.py
+++ predict.py
@@ -14,11 +14,19 @@ class Predict:
     def __init__(self, latest=True, model_index=None):
         if latest:
             self.get_latest_model_path()
+        elif (model_index is not None) and (not latest):
+            self.model_index = model_index
+            self.get_other_models() 
         self.my_model = tf.keras.models.load_model(self.latest_model_path)
 
     def get_latest_model_path(self):
         available_models = os.listdir(config.TRAINED_MODEL_DIR)
         latest_model = sorted(available_models)[-1]
+        self.latest_model_path = os.path.join(config.TRAINED_MODEL_DIR, latest_model)
+
+    def get_other_models(self):
+        available_models = os.listdir(config.TRAINED_MODEL_DIR)
+        latest_model = sorted(available_models)[self.model_index]
         self.latest_model_path = os.path.join(config.TRAINED_MODEL_DIR, latest_model)
 
     def predict(self, input_img_path=None):
